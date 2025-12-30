7c3cd2cf22fca2ab093b10c2bc34ecd67aa55c6c
vasudeva48 <gurram.vasudeva@gmail.com>
2025-12-30 13:49:13 +0530

    Configure dynamic port for deployment

--- backend/server.js
+++ backend/server.js
@@ -63,6 +63,9 @@ app.delete("/tasks/:id", async (req, res) => {
 });
 
 /* ------------------ Server ------------------ */
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
